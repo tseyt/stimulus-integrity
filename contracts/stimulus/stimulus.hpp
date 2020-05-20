8227ad067fd831ae9f24d45ec7e1eeea46c3200b
Taner <tanerseyt@gmail.com>
2020-05-20 14:57:30 -0400

    basic contracts and apiservice.js added

---
+++ contracts/stimulus/stimulus.hpp
@@ -0,0 +1,33 @@
+#include <eosio/eosio.hpp>
+
+using namespace std;
+using namespace eosio;
+class [[eosio::contract]] stimulus : public eosio::contract {
+
+  private:
+    
+    struct survey {
+      int8_t          age = 0;
+    };
+
+    struct [[eosio::table]] user_info {
+      name            username;
+      uint16_t        amount_owed = 0;
+      survey          survey_data;
+
+      auto primary_key() const { return username.value; }
+    };
+
+    typedef eosio::multi_index<name("users"), user_info> users_table;
+    users_table _users;
+    
+
+  public:
+
+    stimulus(name receiver, name code, datastream<const char*> ds ):contract(receiver, code, ds),
+                       _users(receiver, receiver.value) {}
+
+    [[eosio::action]]
+    void login(name username);
+
+};
